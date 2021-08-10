d13cba817917120ed36c71f15b5935109bcdb4bf
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2021-08-10 13:11:42 +0430

    init project for dev environment

---
+++ EurekaDiscoverExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery;
+
+namespace EurekaDiscoverExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        DiscoveryHttpClientHandler _handler;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDiscoveryClient client)
+        {
+            _logger = logger;
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            try
+            {
+                var client = new HttpClient(_handler, false);
+                using (var resp = await client.GetAsync("http://MyEureka_Register_Example_1/WeatherForecast"))
+                {
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        var res2 = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(await resp.Content.ReadAsStringAsync());
+                        return res2;
+                    }
+                }
+                return new List<WeatherForecast>();
+            }
+            catch (Exception wx)
+            {
+                return new List<WeatherForecast>();
+            }
+
+
+
+        }
+    }
+
+
+}
